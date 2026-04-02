174afccdadc09e9d91af65ae257314f20d1e0297
apolloastro322-png <apolloastro322@gmail.com>
2026-04-02 14:02:26 +0700

    templeting

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -37,21 +37,26 @@
 <body>
     <div id="app">
         <div class="main-wrapper main-wrapper-1">
-            <div class="navbar-bg"></div>
-            @include('template.navbar')
-            @include('template.sidebar')
+            @if (!request()->is('/') && !request()->is('register'))
+                <div class="navbar-bg"></div>
+                @include('template.navbar')
+                @include('template.sidebar')
+            @endif
 
             <!-- Main Content -->
             @yield('main-content')
-            <footer class="main-footer">
-                <div class="footer-left">
-                    Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad
-                        Nauval Azhar</a>
-                </div>
-                <div class="footer-right">
+            @if (!request()->is('/') && !request()->is('register'))
+                <footer class="main-footer">
+                    <div class="footer-left">
+                        Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad
+                            Nauval Azhar</a>
+                    </div>
+                    <div class="footer-right">
 
-                </div>
-            </footer>
+                    </div>
+                </footer>
+            @endif
+
         </div>
     </div>
 
